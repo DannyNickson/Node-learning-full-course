7b6d7709ef14bd250f5c65e5f833544b1ccc34a4
anny <puchkovdd@gmail.com>
2022-09-16 13:10:59 +0300

    CRUD creted

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import Posts from './src/Schema/Posts/Posts.js';
+import router from './src/Routs/router.js';
+
 
 const PORT = process.env.PORT || 5000;
-const Application = require('./framework/Application')
-const userRouter = require('./src/user-router')
+const DB_URL = process.env.DB_URL || 'mongodb+srv://puchkovdd:puchkovdd@cluster0.0gxl4ak.mongodb.net/?retryWrites=true&w=majority'
 
-const app = new Application()
-app.addRouter(userRouter);
 
-app.listen(PORT, () => console.log(`Server started om PORT ${PORT}`))
+const app = express()
+
+app.use(express.json())
+app.use('/api', router)
+
+async function startApp() {
+    try {
+        mongoose.connect(DB_URL, { useUnifiedTopology: true, useNewUrlParser: true })
+        app.listen(PORT, () => console.log(`Server started om PORT ${PORT}`))
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+startApp()
+
+
+
